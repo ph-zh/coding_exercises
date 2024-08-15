5531b01ae58fe0b032ac8ac64de8db3ec63ef44e
ph.zh <p.zhavoronkov@gmail.com>
2024-08-15 20:50:09 +0200

    HashMap. Music group task

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,62 @@
 /*
-
+Допишите код приложения, которое позволяет пользователям узнать год основания легендарных музыкальных коллективов,
+а также сохранить информацию о своей любимой группе. Вам нужно объявить и проинициализировать хеш-таблицу, напечатать
+все хранящиеся в ней группы, а также дописать реализацию цифрового меню. Для считывания из консоли пользуйтесь методом nextLine().
+В комментариях прекода оставлены фразы для печати и имена переменных.
  */
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, String> officeTool = new HashMap<>();
-        officeTool.put("A234", "Большой степлер");
-        officeTool.put("Z342", "Чёрно-белый принтер");
-        officeTool.put("X845", "Острые ножницы");
+        ... // Объявите и проиницализируйте хеш-таблицу musicGroups
+        musicGroups.put("The Beatles", 1960);
+        musicGroups.put("Rammstein", 1994);
+        musicGroups.put("Queen", 1970);
+        musicGroups.put("Led Zeppelin", 1968);
+        musicGroups.put("Black Sabbath", 1968);
+        musicGroups.put("AC/DC", 1973);
+        musicGroups.put("The Rolling Stones", 1962);
+        musicGroups.put("The Who", 1964);
+        musicGroups.put("Nightwish", 1996);
+        musicGroups.put("Nirvana", 1987);
+        musicGroups.put("Guns N’ Roses", 1985);
+        musicGroups.put("Scorpions", 1965);
+        musicGroups.put("Kiss", 1973);
+        musicGroups.put("The Doors", 1965);
 
-        for (String inventory : officeTool.keySet()) {
-            System.out.println(inventory);
+        System.out.println("Легендарные группы, год основания которых вы можете узнать:");
+        ... // Распечайте список групп
+        ... // System.out.println(group)
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Выберите опцию");
+            System.out.println("1 - Узнать год основания группы");
+            System.out.println("2 - Добавить любимую группу");
+            System.out.println("3 - Выйти из приложения");
+            int command = Integer.parseInt(scanner.nextLine());
+            if (command == 1) {
+                ... // "Введите название группы"
+                ... // keyGroup
+                ... // year
+               ... // "Год основания " + keyGroup
+                System.out.println(year);
+
+            } else if (command == 2) {
+                ... // "Добавьте свою любимую группу"
+                ... // "Введите её название"
+                ... // nameGroup
+                ... // "Введите год основания"
+                ... // bornYear
+                ...
+                System.out.println("Информация о группе " + nameGroup + " добавлена.");
+
+            } else if (command == 3) {
+              ... // Остановите цикл
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
         }
     }
 }
