bcfa183b613b73705eacfdb0eb88b865f6985382
ph.zh <p.zhavoronkov@gmail.com>
2024-08-15 18:02:26 +0200

    hashmap. KeySet example

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,18 @@
 /*
-В таблице собраны имена клиентов зоомагазина и сумма, на которую они делали заказы.
-Посчитайте, сколько всего денег было потрачено на питомцев.
+
  */
 
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, Double> orders = new HashMap<>();
-        orders.put("Иван И.", 4345.5);
-        orders.put("Ольга С.", 76564.43);
-        orders.put("Александр Т.", 1234.86);
-        orders.put("Александр Р.", 23432.87);
-        orders.put("Екатерина О.", 1034753.6);
-        orders.put("Ярослав В.", 450.0);
+        HashMap<String, String> officeTool = new HashMap<>();
+        officeTool.put("A234", "Большой степлер");
+        officeTool.put("Z342", "Чёрно-белый принтер");
+        officeTool.put("X845", "Острые ножницы");
 
-        double sum = 0; // Объявите переменную, где будет сохранена общая сумма
-        for (double orderSum : orders.values()) { // Пройдитесь в цикле по значениям
-            sum+=orderSum;
+        for (String inventory : officeTool.keySet()) {
+            System.out.println(inventory);
         }
-
-        System.out.println("Всего было совершено заказов на сумму: " + sum);
     }
 }
