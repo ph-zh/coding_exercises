d56449a063fa81c414906ae6832911ec538b836d
ph.zh <p.zhavoronkov@gmail.com>
2024-08-15 15:20:09 +0200

    hashmap task example

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,16 @@
 /*
-Напишите код хеш-таблицы государственных праздников stateHolidays, которая будет содержать месяцы и праздничные даты:
-Январь — 1, 7;
-Февраль — 23;
-Март — 8;
-Май — 1, 9;
-Июнь — 12;
-Ноябрь — 4.
+
  */
 import java.util.HashMap;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, ArrayList<Integer>> stateHolidays = new HashMap<>();
-        ArrayList<Integer> january = new ArrayList<>();
-        january.add(1);
-        january.add(7);
-        stateHolidays.put("Январь", january);
+        HashMap<String, String> officeTool = new HashMap<>();
+        officeTool.put("S234", "Большой степлер");
+        officeTool.put("P342", "Чёрно-белый принтер");
+        officeTool.put("N845", "Острые ножницы");
 
-        ArrayList<Integer> february = new ArrayList<>();
-        february.add(23);
-        stateHolidays.put("Февраль", february);
-
-        ArrayList<Integer> march = new ArrayList<>();
-        march.add(8);
-        stateHolidays.put("Март", march);
-
-        ArrayList<Integer> may = new ArrayList<>();
-        may.add(1);
-        may.add(9);
-        stateHolidays.put("Май", may);
-
-        ArrayList<Integer> june = new ArrayList<>();
-        june.add(12);
-        stateHolidays.put("Июнь", june);
-
-        ArrayList<Integer> november = new ArrayList<>();
-        november.add(4);
-        stateHolidays.put("Ноябрь", november);
-
-        System.out.println(stateHolidays);
+        String tool = officeTool.get("N845");
+        System.out.println(tool); // Получили "Острые ножницы"
     }
 }
