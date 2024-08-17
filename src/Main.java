ff5f02805d3c4b2badcae96aac045494af4c34ae
ph.zh <p.zhavoronkov@gmail.com>
2024-08-17 16:15:53 +0200

    zoo order manager

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,29 @@
 /*
-Допишите код приложения, которое позволяет пользователям узнать год основания легендарных музыкальных коллективов,
-а также сохранить информацию о своей любимой группе. Вам нужно объявить и проинициализировать хеш-таблицу, напечатать
-все хранящиеся в ней группы, а также дописать реализацию цифрового меню. Для считывания из консоли пользуйтесь методом nextLine().
-В комментариях прекода оставлены фразы для печати и имена переменных.
+В хеш-таблице customersOrders класса OrdersManager собрана информация о клиентах зоомагазина (это ключи) и их заказах.
+Дополните код методов:
+1/ В printAllOrders() и getOrdersSum(), которые печатают список всех заказов и возвращают их общую сумму,
+вызовите нужные методы HashMap.
+2/ В методе printCustomerOrders(String customerName) перед печатью заказов конкретного клиента добавьте проверку наличия
+его имени в таблице. Это нужно, чтобы избежать ошибки ссылки на null.
+3/ Метод getMaxOrderCustomerName() должен возвращать имя клиента, который потратил в зоомагазине больше всего.
+Переменные для хранения имени клиента и максимальной суммы заказов мы объявили и инициализировали за вас.
+Числовое переменной присвоили 0, а строковой — пустую строку "". Вам нужно дописать логику работы метода.
+Вы уже не раз искали максимум в массивах или списках. Здесь получится очень похоже!
+4/ Метод removeUnprofitableOrders должен удалять из таблицы клиентов, сумма заказов которых строго меньше 5000.
  */
-import java.util.HashMap;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        // Объявите и проиницализируйте хеш-таблицу musicGroups
-        HashMap<String, Integer> musicGroups = new HashMap<>();
-        musicGroups.put("The Beatles", 1960);
-        musicGroups.put("Rammstein", 1994);
-        musicGroups.put("Queen", 1970);
-        musicGroups.put("Led Zeppelin", 1968);
-        musicGroups.put("Black Sabbath", 1968);
-        musicGroups.put("AC/DC", 1973);
-        musicGroups.put("The Rolling Stones", 1962);
-        musicGroups.put("The Who", 1964);
-        musicGroups.put("Nightwish", 1996);
-        musicGroups.put("Nirvana", 1987);
-        musicGroups.put("Guns N’ Roses", 1985);
-        musicGroups.put("Scorpions", 1965);
-        musicGroups.put("Kiss", 1973);
-        musicGroups.put("The Doors", 1965);
+        OrdersManager ordersManager = new OrdersManager();
 
-        System.out.println("Легендарные группы, год основания которых вы можете узнать:");
-        // Распечайте список групп
-        // System.out.println(group)
-        for(String group : musicGroups.keySet()) {
-            System.out.println(group);
-        }
+        ordersManager.printAllOrders();
+        System.out.println("Всего заказов на сумму: " + ordersManager.getOrdersSum());
 
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Выберите опцию");
-            System.out.println("1 - Узнать год основания группы");
-            System.out.println("2 - Добавить любимую группу");
-            System.out.println("3 - Выйти из приложения");
-            int command = Integer.parseInt(scanner.nextLine());
-            if (command == 1) {
-                // "Введите название группы"
-                // keyGroup
-                // year
-                // "Год основания " + keyGroup
-                System.out.println("Введите название группы");
-                String keyGroup = scanner.nextLine();
-                System.out.println("Год основания " + keyGroup);
-                Integer year = musicGroups.get(keyGroup);
-                System.out.println(year);
+        String maxOrderCustomerName = ordersManager.getMaxOrderCustomerName();
+        System.out.println("Самая большая сумма заказов у " + maxOrderCustomerName);
+        ordersManager.printCustomerOrders(maxOrderCustomerName);
 
-            } else if (command == 2) {
-                // "Добавьте свою любимую группу"
-                // "Введите её название"
-                // nameGroup
-                // "Введите год основания"
-                // bornYear
-                System.out.println("Добавьте свою любимую группу");
-                System.out.println("Введите её название");
-                String nameGroup = scanner.nextLine();
-                System.out.println("Введите год основания");
-                int bornYear = Integer.parseInt(scanner.nextLine());
-                musicGroups.put(nameGroup, bornYear);
-                System.out.println("Информация о группе " + nameGroup + " добавлена.");
-
-            } else if (command == 3) {
-              // Остановите цикл
-                break;
-            } else {
-                System.out.println("Извините, такой команды пока нет.");
-            }
-        }
+        ordersManager.removeUnprofitableOrders();
     }
 }
