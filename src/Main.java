67269ae6499334893ceb84961a678436575a0134
ph.zh <p.zhavoronkov@gmail.com>
2024-08-18 22:45:32 +0200

    nut type task example

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,39 @@
 /*
-В хеш-таблице customersOrders класса OrdersManager собрана информация о клиентах зоомагазина (это ключи) и их заказах.
-Дополните код методов:
-1/ В printAllOrders() и getOrdersSum(), которые печатают список всех заказов и возвращают их общую сумму,
-вызовите нужные методы HashMap.
-2/ В методе printCustomerOrders(String customerName) перед печатью заказов конкретного клиента добавьте проверку наличия
-его имени в таблице. Это нужно, чтобы избежать ошибки ссылки на null.
-3/ Метод getMaxOrderCustomerName() должен возвращать имя клиента, который потратил в зоомагазине больше всего.
-Переменные для хранения имени клиента и максимальной суммы заказов мы объявили и инициализировали за вас.
-Числовое переменной присвоили 0, а строковой — пустую строку "". Вам нужно дописать логику работы метода.
-Вы уже не раз искали максимум в массивах или списках. Здесь получится очень похоже!
-4/ Метод removeUnprofitableOrders должен удалять из таблицы клиентов, сумма заказов которых строго меньше 5000.
+
  */
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        OrdersManager ordersManager = new OrdersManager();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество действий => ");
+        int iterationsCount = scanner.nextInt();
+// ----------- начало видимого кода сниппета
+        for (int counter = 0; counter < iterationsCount; counter++) {
+            System.out.println("1. Фундук");
+            System.out.println("2. Арахис");
+            System.out.println("3. Пекан");
+            System.out.print("Введите тип орехов => ");
+            int nutType = scanner.nextInt();
+            System.out.print("Введите количество грамм => ");
+            if (nutType == 1) {
+                int weight = scanner.nextInt();
+                calculateAndShowNutPrice("фундука", weight, 1680);
+            } else if (nutType == 2) {
+                int weight = scanner.nextInt();
+                calculateAndShowNutPrice("арахиса", weight, 360);
+            } else if (nutType == 3) {
+                int weight = scanner.nextInt();
+                calculateAndShowNutPrice("пекана", weight, 1299);
+            }
+        }
+// ----------- конец видимого кода сниппета
+        System.out.println(nutType);
+    }
 
-        ordersManager.printAllOrders();
-        System.out.println("Всего заказов на сумму: " + ordersManager.getOrdersSum());
-
-        String maxOrderCustomerName = ordersManager.getMaxOrderCustomerName();
-        System.out.println("Самая большая сумма заказов у " + maxOrderCustomerName);
-        ordersManager.printCustomerOrders(maxOrderCustomerName);
-
-        ordersManager.removeUnprofitableOrders();
+    public static void calculateAndShowNutPrice(String nutType, int weight, double price) {
+        System.out.println("Вычисляем стоимость " + weight + " грамма(ов) " + nutType);
+        double total = (weight * price) / 1000;
+        System.out.println("Стоимость " + weight + " грамма(ов) " + nutType + " = " + total + "₽");
     }
 }
