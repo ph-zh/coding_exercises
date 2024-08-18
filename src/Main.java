c00d55223fe4d23ca90af969e39d267e0319503e
ph.zh <p.zhavoronkov@gmail.com>
2024-08-18 22:59:17 +0200

    scope visibility task

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,19 @@
 /*
 
  */
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите количество действий => ");
-        int iterationsCount = scanner.nextInt();
-// ----------- начало видимого кода сниппета
-        for (int counter = 0; counter < iterationsCount; counter++) {
-            System.out.println("1. Фундук");
-            System.out.println("2. Арахис");
-            System.out.println("3. Пекан");
-            System.out.print("Введите тип орехов => ");
-            int nutType = scanner.nextInt();
-            System.out.print("Введите количество грамм => ");
-            if (nutType == 1) {
-                int weight = scanner.nextInt();
-                calculateAndShowNutPrice("фундука", weight, 1680);
-            } else if (nutType == 2) {
-                int weight = scanner.nextInt();
-                calculateAndShowNutPrice("арахиса", weight, 360);
-            } else if (nutType == 3) {
-                int weight = scanner.nextInt();
-                calculateAndShowNutPrice("пекана", weight, 1299);
-            }
-        }
-// ----------- конец видимого кода сниппета
-        System.out.println(nutType);
+        Random random = new Random();
+        System.out.println("Результат умножения = " + multiply(random));
     }
 
-    public static void calculateAndShowNutPrice(String nutType, int weight, double price) {
-        System.out.println("Вычисляем стоимость " + weight + " грамма(ов) " + nutType);
-        double total = (weight * price) / 1000;
-        System.out.println("Стоимость " + weight + " грамма(ов) " + nutType + " = " + total + "₽");
+    static int multiply(Random random) {
+        int argument = random.nextInt(100);
+        System.out.println("Первый аргумент = " + argument);
+        argument = random.nextInt(100);
+        System.out.println("Второй аргумент = " + argument);
+        return argument * argument;
     }
 }
