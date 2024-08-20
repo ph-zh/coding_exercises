e80f4862cec755e1c314f397cb94b2d84ac667d9
ph.zh <p.zhavoronkov@gmail.com>
2024-08-20 11:16:50 +0200

    pizza delivery application task

---
+++ src/BoboPizza.java
@@ -0,0 +1,49 @@
+/*
+Перед вами программа-диспетчер для организации работы курьеров «БобоПицца».
+Она считает время на доставку и предлагает заказ свободному курьеру. На дистанции более 10 км отправляются курьеры на машинах,
+на дистанции менее 2 км курьеры-пешеходы, остальные заказы выполняют курьеры-велосипедисты. Автор программы что-то напутал
+с областями видимости переменных. Исправьте код программы, чтобы она работала корректно.
+ */
+import java.util.Scanner;
+
+public class BoboPizza {
+    private Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        List<BikeTimeCalculator> bikes = new ArrayList<BikeTimeCalculator>() {{
+            add(new BikeTimeCalculator("Иван"));
+            add(new BikeTimeCalculator("Василий"));
+            add(new BikeTimeCalculator("Инга"));
+        }};
+        List<CarTimeCalculator> cars = new ArrayList<CarTimeCalculator>() {{
+            add(new CarTimeCalculator("Ольга"));
+            add(new CarTimeCalculator("Пётр"));
+        }};
+        List<HumanTimeCalculator> humans = new ArrayList<HumanTimeCalculator>() {{
+            add(new HumanTimeCalculator("Анна"));
+            add(new HumanTimeCalculator("Дмитрий"));
+            add(new HumanTimeCalculator("Алексей"));
+        }};
+        new BoboPizza().loop();
+    }
+
+    private void loop() {
+        CourierSelector selector = new CourierSelector(bikes, cars, humans);
+
+        DeliveryInfo delivery = null;
+
+        do {
+            System.out.println("Введите расстояние в метрах => ");
+            int distance = scanner.nextInt();
+            delivery = selector.selectDelivery(distance);
+            if (delivery != null) {
+                System.out.println(
+                        "Пицца будет доставлена через " + delivery.getTime() + " минут(у,ы)."
+                );
+                System.out.println("Ваш курьер - " + delivery.getName() + ".");
+            } else {
+                System.out.println("Доступных курьеров не найдено");
+            }
+        } while (delivery != null);
+    }
+}
