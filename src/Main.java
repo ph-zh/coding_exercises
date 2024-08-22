aa2a11d12d7c428a33764067ab987fe215c3f42c
ph.zh <p.zhavoronkov@gmail.com>
2024-08-22 13:18:02 +0200

    import util library example

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,13 @@
 /*
 
  */
+import java.util.HashMap;
 
 public class Main {
-
     public static void main(String[] args) {
-        String language = "Java";
-
-        switch (language) {
-            case "Java":
-                System.out.println("Java — это язык для всего.");
-                break;
-            case "Python":
-                System.out.println("На Python часто обрабатывают данные и разрабатывают нейронные сети.");
-                break;
-            case "JavaScript":
-                System.out.println("JavaScript - отличный выбор для веб-разработки");
-                break;
-            default:
-                System.out.println("Про язык программирования " + language + " мы ничего не знаем.");
-        }
+        HashMap<String, String> capitals = new HashMap<>();
+        capitals.put("Россия", "Москва");
+        capitals.put("Италия", "Рим");
+        System.out.println("Столица России - " + capitals.get("Россия"));
     }
 }
