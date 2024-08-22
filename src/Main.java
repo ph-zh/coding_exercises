eced581c34bd83621e7db9553d8c48908611ad92
ph.zh <p.zhavoronkov@gmail.com>
2024-08-23 00:11:07 +0200

    repository connection

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 /*
 
  */
-import com.offer.special;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Первый проект!");
+        HashMap<String, String> capitals = new HashMap<>();
+        capitals.put("Россия", "Москва");
+        capitals.put("Италия", "Рим");
+        System.out.println("Столица России - " + capitals.get("Россия"));
     }
 }
