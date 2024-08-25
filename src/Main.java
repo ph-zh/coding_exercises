8634c54db7a6dd16f37c1b5ba21e6a858a3c9971
ph.zh <p.zhavoronkov@gmail.com>
2024-08-25 22:39:39 +0200

    access modifier

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,35 @@
 /*
-Исправьте ошибку доступа в классе Practicum.
+Коллега помогал главному бухгалтеру и написал калькулятор рабочих часов, но не доделал работу и ушёл в отпуск.
+Сотрудники компании рискуют остаться без выплат! Доработайте приложение и расставьте модификаторы доступа в соответствии с логикой приложения.
+Доступ к рабочим часам и имени работника нужно закрыть, чтобы уберечь эти данные. А вот доступ к повышающему коэффициенту можно сделать максимально
+широким. Чтобы избежать ошибок при компиляции, конструктор класса WorkCalculator тоже лучше сделать доступным. Кроме того, нужно предусмотреть
+публичный доступ к интерфейсу калькулятора — всем его методам.
  */
+
+package ru.yandex.practicum;
+
+import ru.yandex.practicum.work.WorkCalculator;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Product product = new Product("Пирожок", 35.0);
-        System.out.println("Стоимость продукта - " + product.getPrice() + " руб.");
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя работника:");
+        WorkCalculator calculator = new WorkCalculator(scanner.next());
 
-class Product {
-    private String name;
-    private double price;
+        System.out.println("Введите коэффициент в формате 1,5:");
+        calculator.coefficient = scanner.nextDouble();
 
-    public Product(String nameProduct, double priceProduct) {
-        name = nameProduct;
-        price = priceProduct;
-    }
+        System.out.println("Введите количество отработанных дней без коэффициента:");
+        double hoursWithoutCoefficient = calculator.calculate(scanner.nextInt());
 
-    public double getPrice() {
-        return price;
+        System.out.println("Введите количество отработанных дней с коэффициентом:");
+        double hoursWithCoefficient = calculator.calculateWithCoefficient(scanner.nextInt());
+
+        double allWorkHours = hoursWithoutCoefficient + hoursWithCoefficient;
+        // метод printf позволяет нам вывести строку с форматированием
+        // символ %s заменит строку, а %.2f число с 2 знаками после запятой
+        System.out.printf("Работник %s отработал %.2f часов.",
+                calculator.getWorkerName(), allWorkHours);
     }
 }
