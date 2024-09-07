c1053d10ade3033ee323a32344ca7517621fc408
ph.zh <p.zhavoronkov@gmail.com>
2024-09-07 14:21:58 +0200

    inheritance task example #2

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,56 +1,32 @@
 /*
-Вернёмся к примеру. Созданный нами класс Fox наследник и Canidae, и Animal. Его объекты могут иметь рост,
-вес и кричать «Я животное!», быть хищниками и рычать, а также отличаться своим цветом. При этом нам не пришлось трижды
- дублировать один и тот же код.  Посмотрим, как это работает — создадим объект класса Fox:
  */
+class Coffee {
+    protected double espresso;
+    protected double milk;
+
+    public Coffee() {
+        espresso = 50.0;
+        milk = 250.0;
+    }
+
+    public double mixAndGetVolume() {
+        return espresso + milk;
+    }
+}
+
+class Cappuccino extends Coffee {
+}
+
+class Latte extends Coffee {
+}
+
 public class Practicum {
     public static void main(String[] args) {
-        Fox foxAlica = new Fox();
-        System.out.println("Это лиса Алиса");
-        System.out.println("Цвет - " + foxAlica.color); // поле класса Fox
-        System.out.println("Вес - " + foxAlica.weight + " кг"); // конструктор Fox
-        System.out.println("Она дикая - " + foxAlica.isWild); // поле класса Animal
-        System.out.println("Хищник - " + foxAlica.isPredator); // поле класса Canidae
-        System.out.println("Она умеет говорить - " + foxAlica.say()); // метод класса Animal
-        System.out.println("И может зарычать " + foxAlica.growl()); // метод класса Canidae
-    }
-}
-
-class Animal {
-    protected double weight;
-    protected int age;
-    protected int heartRate;
-    protected boolean isWild;
-
-    public Animal() {
-        weight = 0.0;
-        age = 0;
-        heartRate = 100;
-        isWild = true;
-    }
-
-    public String say() {
-        return "Я животное!";
-    }
-}
-
-class Canidae extends Animal { // здесь происходит наследование
-    protected boolean isPredator;
-
-    public Canidae() {
-        isPredator = true;
-    }
-
-    public String growl() {
-        return "Р-р-р-р-р!";
-    }
-}
-
-class Fox extends Canidae { // здесь происходит наследование и Canidae, и Animal
-    protected String color;
-
-    public Fox() {
-        color = "рыжий";
-        weight = 2.0;
+        Cappuccino cappuccino = new Cappuccino();
+        // напрямую используем метод суперкласса:
+        System.out.println("Объём чашки капучино - " + cappuccino.mixAndGetVolume());
+        Latte latte = new Latte();
+        // напрямую используем переменную суперкласса:
+        System.out.println("Объём эспрессо в латте - " + latte.espresso);
     }
 }
