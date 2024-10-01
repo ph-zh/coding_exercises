12edf35848e5616d9bf0e760b1c57c29d73f99a0
ph.zh <p.zhavoronkov@gmail.com>
2024-10-01 22:25:55 +0200

    task with super class example 2

--- src/Practicum.java
+++ src/Practicum.java
@@ -3,25 +3,23 @@
  */
 
 class Teacher {
-
-    public void startLesson() {
-        System.out.println("Звонок для кого прозвенел?!");
-    }
+    int numberOfLessons = 34; // поле класса-родителя скрыто
 }
 
 class GymTeacher extends Teacher {
+    int numberOfLessons = 3;
 
-    @Override
-    public void startLesson() {
-        super.startLesson(); //вызов метода класса-родителя
-        System.out.println("Стройтесь по росту!");
+    public void printSchedule() {
+        // печатаем значение поля суперкласса
+        System.out.println("Число уроков в неделю - " + super.numberOfLessons);
+        // печатаем значение поля подкласса
+        System.out.println("Число уроков физкультуры - " + numberOfLessons);
     }
 }
 
 public class Practicum {
     public static void main(String[] args) {
         GymTeacher teacher = new GymTeacher();
-        System.out.println("Учитель физкультуры говорит:");
-        teacher.startLesson();
+        teacher.printSchedule();
     }
 }
