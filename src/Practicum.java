4d523a42f149ecb45ab7e13df19399d5702e64a1
ph.zh <p.zhavoronkov@gmail.com>
2024-10-01 23:09:01 +0200

    task without super class initialization example 2

--- src/Practicum.java
+++ src/Practicum.java
@@ -4,19 +4,21 @@
 
 class Teacher {
     String name;
-    // констуктора без параметров (default) нет
-    public Teacher(String newName) { // объявлен конструктор с параметром
+    public Teacher(String newName) {
         name = newName;
     }
 }
 
 class LiteratureTeacher extends Teacher {
-    public LiteratureTeacher() { // тут произойдёт ошибка компиляции
+    public LiteratureTeacher() {
+        super("Мария Ивановна Петрова"); // вызвали родительский констуктор
     }
 }
 
 public class Practicum {
     public static void main(String[] args) {
         LiteratureTeacher teacher = new LiteratureTeacher();
+        // объект создан в классе-наследнике при помощи конструктора класса-родителя
+        System.out.println("Учитель литературы в 11-Б - " + teacher.name);
     }
 }
