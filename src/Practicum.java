120fc5141980170ef63022a96c120a7d4c0458f3
ph.zh <p.zhavoronkov@gmail.com>
2024-10-01 23:00:57 +0200

    task without super class initialization example

--- src/Practicum.java
+++ src/Practicum.java
@@ -3,29 +3,20 @@
  */
 
 class Teacher {
-    int numberOfPupils; // число учеников в классе
-    double workLoad; // нагрузка учителя
-
-    public Teacher() {
-        numberOfPupils = 30;
-        workLoad = 22;
+    String name;
+    // констуктора без параметров (default) нет
+    public Teacher(String newName) { // объявлен конструктор с параметром
+        name = newName;
     }
 }
 
-class BiologyTeacher extends Teacher {
-    int numberOfLabs; // число лабораторных работ
-
-    public BiologyTeacher() {
-        super(); // здесь явно вызван конструктор суперкласса
-        numberOfLabs = 10;
+class LiteratureTeacher extends Teacher {
+    public LiteratureTeacher() { // тут произойдёт ошибка компиляции
     }
 }
 
 public class Practicum {
     public static void main(String[] args) {
-        BiologyTeacher teacher = new BiologyTeacher();
-        System.out.println("Число учеников на уроке биологии - " + teacher.numberOfPupils);
-        System.out.println("Рабочая нагрузка " + teacher.workLoad + " часов в месяц");
-        System.out.println("Количество лабораторных работ - " + teacher.numberOfLabs);
+        LiteratureTeacher teacher = new LiteratureTeacher();
     }
 }
